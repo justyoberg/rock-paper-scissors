498576dc0f3b9b33bc90194dad737161f3c19a98
Justy Oberg <justyoberg1@gmail.com>
2022-09-12 14:04:37 -0700

    Refactor playRound function and create game function

--- scripts.js
+++ scripts.js
@@ -4,36 +4,33 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, compSelection) {
-  
-  if (playerSelection.toLowerCase() === "rock") {
-    if (compSelection === "scissors") {
-      return "You win! Rock beats Scissors!";
-    } else if (compSelection === "paper") {
-      return "You lose! Paper beats Rock!";
-    } else {
-      return "It was a tie!";
-    }
-  } else if (playerSelection.toLowerCase() === "paper") {
-    if (compSelection === "rock") {
-      return "You win! Paper beats Rock!";
-    } else if (compSelection === "scissors") {
-        return "You lose! Scissors beats Paper!";
-    } else {
-      return "It was a tie!";
-    } 
-  } else if (playerSelection.toLowerCase() === "scissors") {
-    if (compSelection === "paper") {
-      return "You win! Scissors beats Paper!";
-    } else if (compSelection === "rock") {
-      return "You lose! Rock beats Scissors!";
-    } else {
-      return "It was a tie!";
-    }
-  } else {
-      return "Please enter a valid move."
-  }
-
+  if (playerSelection === "paper" && compSelection === "rock") {
+    return true;
+  } else if (playerSelection === "rock" && compSelection === "scissors") {
+    return true;
+  } else if (playerSelection === "scissors" && compSelection === "paper") {
+    return true;
+  } 
+  return false;
 }
 
-const playerMove = prompt("Enter a move: ");
-console.log(playRound(playerMove, getComputerChoice()));
+
+function game() {
+  let score = 0;
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt("Enter a move: (rock paper scissors)");
+    if (playRound(playerChoice, getComputerChoice())) {
+      score++;
+      console.log(`Gained a point! Score: ${score}`)
+    } else {
+      console.log("You either lost, or it was a tie! No points!")
+    }
+  }
+  if (score >= 3) {
+    console.log(`You won with a score of ${score}!`)
+  } else {
+    console.log(`You lost with a score of ${score}!`)
+  }
+}
+
+game();
