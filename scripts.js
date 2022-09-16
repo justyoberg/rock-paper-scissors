a5937f202128d01a306c06d659fa8b0fb0256836
Justy Oberg <justyoberg1@gmail.com>
2022-09-16 15:23:27 -0700

    Create buttons for user input

--- scripts.js
+++ scripts.js
@@ -24,24 +24,40 @@ function checkWin(playerScore, compScore) {
   }
 }
 
-function game() {
-  let score = 0;
-  let compScore = 0;
-  // Main game loop
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Enter a move: (rock paper scissors)");
-    if (playRound(playerChoice, getComputerChoice()) === true) {
-      score++;
-      console.log(`You won! Scores: P:${score} C:${compScore}`);
-    } else if (playRound(playerChoice, getComputerChoice()) === "tie") {
-      console.log(`It was a tie! Scores: P:${score} C:${compScore}`)
-    }
-    else {
-      compScore++;
-      console.log(`You lost! Scores: P:${score} C:${compScore}`);
-    }
-  }
-  checkWin(score, compScore);
-}
+// function game() {
+//   let score = 0;
+//   let compScore = 0;
+//   // Main game loop
+//   for (let i = 0; i < 5; i++) {
+//     let playerChoice = prompt("Enter a move: (rock paper scissors)");
+//     if (playRound(playerChoice, getComputerChoice()) === true) {
+//       score++;
+//       console.log(`You won! Scores: P:${score} C:${compScore}`);
+//     } else if (playRound(playerChoice, getComputerChoice()) === "tie") {
+//       console.log(`It was a tie! Scores: P:${score} C:${compScore}`)
+//     }
+//     else {
+//       compScore++;
+//       console.log(`You lost! Scores: P:${score} C:${compScore}`);
+//     }
+//   }
+//   checkWin(score, compScore);
+// }
 
-game();
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.innerText = "Rock";
+paper.innerText = "Paper";
+scissors.innerText = "Scissors";
+
+document.body.appendChild(rock);
+document.body.appendChild(paper);
+document.body.appendChild(scissors);
+
+rock.addEventListener("click", () => playRound("rock", getComputerChoice));
+paper.addEventListener("click", () => playRound("paper", getComputerChoice));
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice));
+
+// game();
