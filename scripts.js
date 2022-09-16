c6dfc368cffe0d38f289b197c0f6ee9c4fe18661
Justy Oberg <justyoberg1@gmail.com>
2022-09-16 14:23:47 -0700

    Add a checkWin function

--- scripts.js
+++ scripts.js
@@ -1,36 +1,47 @@
+const choices = ["rock", "paper", "scissors"]; 
+
 function getComputerChoice() {
-  let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, compSelection) {
-  if (playerSelection === "paper" && compSelection === "rock") {
-    return true;
-  } else if (playerSelection === "rock" && compSelection === "scissors") {
-    return true;
-  } else if (playerSelection === "scissors" && compSelection === "paper") {
+  if (playerSelection === compSelection) return "tie";
+  if ((playerSelection === "paper" && compSelection === "rock") ||
+     (playerSelection === "rock" && compSelection === "scissors") || 
+     (playerSelection === "scissors" && compSelection === "paper")) {
     return true;
   } 
   return false;
 }
 
+function checkWin(playerScore, compScore) {
+  if (playerScore > compScore) {
+    console.log(`You won! Final scores: P:${playerScore} C:${compScore}`);
+  } else if (playerScore === compScore) {
+    console.log(`It was a tie! Final scores: P:${playerScore} C:${compScore}`);
+  } else {
+    console.log(`You lost! Final scores: P:${playerScore} C:${compScore}`)
+  }
+}
 
 function game() {
   let score = 0;
+  let compScore = 0;
+  // Main game loop
   for (let i = 0; i < 5; i++) {
     let playerChoice = prompt("Enter a move: (rock paper scissors)");
-    if (playRound(playerChoice, getComputerChoice())) {
+    if (playRound(playerChoice, getComputerChoice()) === true) {
       score++;
-      console.log(`Gained a point! Score: ${score}`)
-    } else {
-      console.log("You either lost, or it was a tie! No points!")
+      console.log(`You won! Scores: P:${score} C:${compScore}`);
+    } else if (playRound(playerChoice, getComputerChoice()) === "tie") {
+      console.log(`It was a tie! Scores: P:${score} C:${compScore}`)
+    }
+    else {
+      compScore++;
+      console.log(`You lost! Scores: P:${score} C:${compScore}`);
     }
   }
-  if (score >= 3) {
-    console.log(`You won with a score of ${score}!`)
-  } else {
-    console.log(`You lost with a score of ${score}!`)
-  }
+  checkWin(score, compScore);
 }
 
 game();
